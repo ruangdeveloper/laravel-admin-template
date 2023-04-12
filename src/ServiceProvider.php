a35bec9cead2aa09259b70e9e97108dbf1e8d931
kykurniawan <contact.rizkykurniawan@gmail.com>
2023-04-12 13:24:17 +0800

    initial commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace RuangDeveloper\LaravelAdminTemplate;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    public function register()
+    {
+        // register any application services
+    }
+
+    public function boot()
+    {
+        // boot any application services
+    }
+}
